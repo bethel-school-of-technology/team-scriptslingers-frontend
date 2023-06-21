d06803941ac39e7fc914636f3216d104c9e49143
Marina-Guderian <113396529+Marina-Guderian@users.noreply.github.com>
2023-06-21 19:39:24 -0400

    Update event model with eventDate

--- src/app/models/event.ts
+++ src/app/models/event.ts
@@ -4,12 +4,14 @@ export class Event {
     description?: string;
     location?: string;
     hostName?: string;
+    eventDate?: Date;
 
-    constructor(eventId?: string, eventTitle?: string, description?: string, location?: string, hostName?: string) {
+    constructor(eventId?: string, eventTitle?: string, description?: string, location?: string, hostName?: string, eventDate?: Date) {
         this.eventId = eventId;
         this.eventTitle = eventTitle;
         this.description = description;
         this.location = location;
         this.hostName = hostName;
+        this.eventDate = eventDate;
     }
 }
