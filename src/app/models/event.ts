ce59fd8521bcc1826569105e2de549f2a1707def
Marina <marina.guderian@gmail.com>
2023-06-23 22:59:38 -0400

    changed IsFinished to isFinished

--- src/app/models/event.ts
+++ src/app/models/event.ts
@@ -5,15 +5,15 @@ export class Event {
     location?: string;
     hostName?: string;
     eventTime?: Date;
-    IsFinished?: boolean;
+    isFinished?: boolean;
 
-    constructor(eventId?: string, eventTitle?: string, description?: string, location?: string, hostName?: string, eventTime?: Date, IsFinished?: boolean) {
+    constructor(eventId?: string, eventTitle?: string, description?: string, location?: string, hostName?: string, eventTime?: Date, isFinished?: boolean) {
         this.eventId = eventId;
         this.eventTitle = eventTitle;
         this.description = description;
         this.location = location;
         this.hostName = hostName;
         this.eventTime = eventTime;
-        this.IsFinished = IsFinished;
+        this.isFinished = isFinished;
     }
 }
