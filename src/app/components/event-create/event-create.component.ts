ec43d6a942d00a3590285de7a73401cde06c5ba9
Joseph Streubel <joseph.streubel@gmail.com>
2023-06-28 22:26:51 -0500

    updated event create and edit components, no authentication

--- src/app/components/event-create/event-create.component.ts
+++ src/app/components/event-create/event-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Event } from 'src/app/models/event';
+import { EventService } from 'src/app/services/event.service';
 
 @Component({
   selector: 'app-event-create',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-create.component.css']
 })
 export class EventCreateComponent implements OnInit {
+  eventId: number = 0;
+  newEvent: Event = new Event(this.eventId);
+  inputs = [
+    { placeholder: 'Event Description', value: '' },
+    { placeholder: 'Event Title', value: '' },
+    { placeholder: 'Where is the event being held?', value: '' },
+    { placeholder: 'When is the event?', value: '' }
+];
 
-  constructor() { }
+  constructor(private eventService: EventService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  addEvent(){
+    this.eventService.createEvent(this.newEvent).subscribe(() => {
+      window.alert("Posted Successfully");
+      this.router.navigate(['calendar'])
+    }, error => {
+      console.log('Error: ', error)
+      if (error.status === 401 || error.status === 403) {
+        this.router.navigate(['signin']);
+      }
+    })
+  }
 }
