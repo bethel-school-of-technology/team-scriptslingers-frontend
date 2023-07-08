045c30663d5c1c2b0eae45abcbafc67af1ad5c4f
Joseph Streubel <joseph.streubel@gmail.com>
2023-07-07 20:32:08 -0500

    Nav to display First Last name from token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from './services/user.service';
 import { User } from './models/user';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-root',
@@ -14,18 +15,27 @@ export class AppComponent {
   username?: string;
   userList: User[] = [];
   currentUserEmail: string | null = null;
+  firstName: string | undefined;
+  lastName: string | undefined;
 
-  constructor(public router: Router, public userService: UserService) {}
+  constructor(public router: Router, public userService: UserService) { }
 
   ngOnInit(): void {
-     this.userService.currentUserEmail$.subscribe(email => {
+    this.userService.currentUserEmail$.subscribe(email => {
       this.currentUserEmail = email;
     });
+
+    const token = localStorage.getItem('myEventToken');
+    if (token) {
+      const decodedToken: any = jwt_decode(token);
+      this.firstName = decodedToken.given_name;
+      this.lastName = decodedToken.family_name;
+    }
   }
 
   logout() {
     this.userService.logout()
     location.reload();
   }
-  
+
 }
