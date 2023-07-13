af5462dedf56b037366bcd88a8f750515045453f
Marina <marina.guderian@gmail.com>
2023-07-12 20:09:30 -0400

    updating calendar page

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EventService } from 'src/app/services/event.service';
+import { UserService } from 'src/app/services/user.service';
+import { Event } from 'src/app/models/event';
 
 @Component({
   selector: 'app-calendar',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
+  eventList: Event[] = [];
 
-  constructor() { }
+  constructor(private eventService: EventService, public userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+    this.eventService.getAllFutureEvent().subscribe(events => {
+      console.log(events);
+      this.eventList = events;
+    });
   }
 
 }
